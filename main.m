7adc9c3783dc6d8c0b90c37c2dc91c9ef20aca26
zehra-bak <104216027+zehra-bak@users.noreply.github.com>
2024-07-28 00:37:10 +0300

    VLFeat kütüphanesi yüklendi.

--- main.m
+++ main.m
@@ -1,3 +1,6 @@
+% VLFeat kütüphanesini yükleme
+run('C:\Users\pc\Desktop\Downloads\vlfeat-0.9.21\toolbox\vl_setup.m');
+
 % Kalibrasyon verilerini yükleme
 load('calibrationSession1.mat', 'calibrationSession');
 
@@ -22,33 +25,35 @@ title('Original (left) vs. Undistorted (right) Image 1');
 figure;
 imshowpair(fisheyeImage2, undistortedImage2, 'montage');
 title('Original (left) vs. Undistorted (right) Image 2');
+
 % Grayscale'e dönüştürme
-grayImage1 = rgb2gray(undistortedImage1);
-grayImage2 = rgb2gray(undistortedImage2);
+grayImage1 = single(rgb2gray(undistortedImage1));
+grayImage2 = single(rgb2gray(undistortedImage2));
 
-% SIFT algoritması ile özellikleri algılama
-points1 = detectSURFFeatures(grayImage1);
-points2 = detectSURFFeatures(grayImage2);
-
-% Özellik tanımlayıcılarını çıkarma
-[features1, validPoints1] = extractFeatures(grayImage1, points1);
-[features2, validPoints2] = extractFeatures(grayImage2, points2);
+% SIFT özelliklerini tespit etme
+[frames1, descriptors1] = vl_sift(grayImage1);
+[frames2, descriptors2] = vl_sift(grayImage2);
 
 % Özellik eşleştirme
-indexPairs = matchFeatures(features1, features2, 'MaxRatio', 0.9, 'MatchThreshold', 100);
+[matches, scores] = vl_ubcmatch(descriptors1, descriptors2);
 
 % Eşleşen noktaları elde etme
-matchedPoints1 = validPoints1(indexPairs(:, 1));
-matchedPoints2 = validPoints2(indexPairs(:, 2));
+matchedPoints1 = frames1(1:2, matches(1,:))';
+matchedPoints2 = frames2(1:2, matches(2,:))';
+
+% Yeterli sayıda eşleşen nokta olup olmadığını kontrol etme
+if size(matchedPoints1, 1) < 4 || size(matchedPoints2, 1) < 4
+    error('Yeterli sayıda eşleşen nokta bulunamadı.');
+end
 
 % Eşleşen noktaları görselleştirme
 figure;
-showMatchedFeatures(undistortedImage1, undistortedImage2, matchedPoints1, matchedPoints2);
+showMatchedFeatures(undistortedImage1, undistortedImage2, matchedPoints1, matchedPoints2, 'montage');
 title('Matched Points');
 
 % Homografi matrisini tahmin etme
-   [tform, inlierPoints1, inlierPoints2] = estimateGeometricTransform(matchedPoints1, matchedPoints2, 'projective', ...
-        'MaxDistance', 2, 'Confidence', 99.9, 'MaxNumTrials', 10000);
+[tform, inlierPoints1, inlierPoints2] = estimateGeometricTransform2D(matchedPoints1, matchedPoints2, 'projective', ...
+    'MaxDistance', 3, 'Confidence', 99, 'MaxNumTrials', 10000);
 
 % Geometrik dönüşüm matrisini inceleme
 disp('Homografi matrisi:');
@@ -64,7 +69,7 @@ outputView = imref2d(imageSize);
 warpedImage1 = imwarp(undistortedImage1, tform, 'OutputView', outputView);
 
 % İkinci görüntüyü de aynı kanvasa yerleştirme
-warpedImage2 = undistortedImage2;
+warpedImage2 = imwarp(undistortedImage2, tform, 'OutputView', outputView);
 
 % İki görüntüyü birleştirme
 stitchedImage = max(warpedImage1, warpedImage2);
